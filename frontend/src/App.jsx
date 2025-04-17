8008af1d17867cf3979106162abc6acabf8bdad4
Matheus Dantas <65237455+oMaDaSa@users.noreply.github.com>
2025-04-17 20:08:31 -0300

    base url now redirects to /persons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import HeaderComponent from './components/HeaderComponent';
 import ListPersonComponent from './components/ListPersonComponent';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate} from 'react-router-dom';
 import PersonComponent from './components/PersonComponent';
 import TransactionComponent from './components/TransactionComponent';
 import ListPersonTransactionsComponent from './components/ListPersonTransactionsComponent';
@@ -13,14 +13,18 @@ function App() {
     <BrowserRouter>
       <HeaderComponent />
       <Routes>
-        {/* http:localhost:3000 */}
-        <Route path='/' element = { <ListPersonComponent />}></Route>
+        {/* http:localhost:3000 -> redireciona para /persons*/}
+        <Route path='/' element = { <Navigate to="/persons" replace />}></Route>
+
         {/* http:localhost:3000/persons */}
         <Route path='/persons' element = { <ListPersonComponent />}></Route>
+
         {/* http:localhost:3000/add-person */}
         <Route path='/persons/add-person' element = { <PersonComponent />}></Route>
+
         {/* http:localhost:3000/add-transaction */}
         <Route path='/persons/add-transaction' element = { <TransactionComponent />}></Route>
+        
         {/* http:localhost:3000/person-transactions */}
         <Route path='/persons/person-transactions/:id' element = { <ListPersonTransactionsComponent />}></Route>
       </Routes>
